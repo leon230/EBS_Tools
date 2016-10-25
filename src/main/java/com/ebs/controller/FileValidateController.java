34b83867d364219369084759c526b87442138f3b
lukasz.homik <homik.lukasz@gmail.com>
2016-10-25 10:06:16 +0200

    Before release. Working.

--- src/main/java/com/ebs/controller/FileValidateController.java
+++ src/main/java/com/ebs/controller/FileValidateController.java
@@ -7,13 +7,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 
-/**
- * Created by lukasz.homik on 2016-09-14.
- */
 @Controller
 public class FileValidateController {
 
