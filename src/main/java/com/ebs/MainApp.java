bf43bb310e50ba8d1e6c662471dd821fd3e90a4e
Leon <homik.lukasz@gmail.com>
2016-10-26 15:32:00 +0200

    Junit.

--- src/main/java/com/ebs/MainApp.java
+++ src/main/java/com/ebs/MainApp.java
@@ -2,17 +2,34 @@ package com.ebs;
 
 import com.ebs.storage.StorageProperties;
 import com.ebs.storage.StorageService;
+import com.ebs.test.TestClass;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
 public class MainApp {
 
     public static void main(String[] args) {
+/**
+ * Test class run
+ */
+        Result result = JUnitCore.runClasses(TestClass.class);
+
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+
+        System.out.println(result.wasSuccessful());
+/**
+ * Main App run
+ */
         SpringApplication.run(MainApp.class, args);
 
         System.out.println("Starting...");
